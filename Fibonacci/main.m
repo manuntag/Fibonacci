fc1c1bf1f22cfc13b0773378685368015648ada4
manuntag <manuntag@ualberta.ca>
2015-01-14 17:09:27 -0800

    Initial Commit

--- Fibonacci/main.m
+++ Fibonacci/main.m
@@ -8,10 +8,58 @@
 
 #import <Foundation/Foundation.h>
 
+int i;
+
+int zero = 0;
+int first = 1;
+int second = 0;
+
+
+//0 1 1 2 3 5 8 13 21 34 55
+
+// using iteration
+
+int fibonacci (int numbers) {
+    
+    for (i=3; i<=numbers; i++) {
+        
+        second = zero + first;
+        zero = first;
+        first = second;
+    }
+    
+    return second;
+}
+
+
+
+// fibonacci recursion, pass by value
+
+
+int recursiveValueFibonacci(int remainingSteps, int previousNumber, int previousPreviousNumber) {
+    
+    if (remainingSteps<=0) {
+    
+        NSLog(@"Fibonacci number has been found");
+        return previousNumber;
+        
+    }
+    
+    int solution = recursiveValueFibonacci(remainingSteps-1, previousPreviousNumber, previousNumber+previousPreviousNumber);
+    return solution;
+}
+
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        int result = fibonacci(11);
+        printf("%d", result);
+        
+        int result2 = recursiveValueFibonacci (6,0,1);
+        printf("%d", result2);
+        
     }
     return 0;
 }
